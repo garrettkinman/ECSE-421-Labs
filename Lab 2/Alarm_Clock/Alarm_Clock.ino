e68f311b9cc9be37034efa11deb1bf023dcb1be3
garrettkinman <garrettkinman@gmail.com>
2021-02-26 03:26:35 -0500

    added global variables, declarations

--- Lab 2/Alarm_Clock/Alarm_Clock.ino
+++ Lab 2/Alarm_Clock/Alarm_Clock.ino
@@ -1,23 +1,42 @@
 #include "Arduino_SensorKit.h"
 
+// pin declarations
 #define BUTTON 4
 #define BUZZER 5
 #define POTENTIOMETER A0
 
+// state types
+#define NORMAL 1
+#define PROGRAMMING 2
+#define ALARM 3
+#define FIRST_PRESS 4
+#define SNOOZE 5
+
+// global variables
+int button = 0;
+unsigned long currentTime = millis();
+unsigned long alarmTime = 0;
+unsigned long buttonTime = 0;
+
+char* formatTime(unsigned long ms) {
+  // TODO
+}
+
 void setup() {
-  Oled.begin();
-  Oled.setFlipMode(true);
   pinMode(BUTTON, INPUT);
   pinMode(POTENTIOMETER, INPUT);
   pinMode(BUZZER, OUTPUT);
+  Oled.begin();
+  Oled.setFlipMode(true);
 }
 
 void loop() {
-  int random_value = random(0, 1023);   // create a random value
+  button = digitalRead(BUTTON);
+  currentTime = millis();
 
   Oled.setFont(u8x8_font_chroma48medium8_r); 
   Oled.setCursor(0, 3);
   Oled.print("Value: ");
   Oled.print(random_value);
-  delay(1000);
+  delay(250);
 }
