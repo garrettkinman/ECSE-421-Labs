f0f52df8735ac0b3cefc9428a05f184fe22ff484
garrettkinman <garrettkinman@gmail.com>
2021-03-22 02:00:37 -0400

    Create blink_timer_isr.ino

---
+++ Lab 3/blink_timer_isr/blink_timer_isr.ino
@@ -0,0 +1,16 @@
+#include "Arduino_SensorKit.h"
+
+#define LED 6
+
+void setup() {
+  // initialize pins for IO
+  pinMode(LED, OUTPUT);
+
+  // for debug purposes
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
