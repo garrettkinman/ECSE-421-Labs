3147f01f6afc94646b5675cc8abdf3abbf4f623e
garrettkinman <garrettkinman@gmail.com>
2021-03-22 00:54:47 -0400

    exercise 1 working

--- Lab 3/isr_led_button/isr_led_button.ino
+++ Lab 3/isr_led_button/isr_led_button.ino
@@ -3,23 +3,30 @@
 #define BUTTON 2
 #define LED 6
 
+volatile byte state = LOW;
+
 void setup() {
   // initialize pins for IO
   pinMode(LED, OUTPUT);
   pinMode(BUTTON, INPUT);
-  attachInterrupt(digitalPinToInterrupt(BUTTON, turnOn, RISING);
-  attachInterrupt(digitalPinToInterrupt(BUTTON, turnOff, FALLING);
+
+  // attach the interrupt to trigger on a button press or release
+  // note that sometimes it double detects a press or release; this is accounted for in the ISR to still be correct
+  attachInterrupt(digitalPinToInterrupt(BUTTON), blink, CHANGE);
+
+  // for debug purposes
+  Serial.begin(115200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  delay(100);
 }
 
-void turnOn() {
-  digitalWrite(LED, HIGH);
-}
-
-void turnOff() {
-  digitalWrite(LED, LOW);
+// ISR
+void blink() {
+  // digital read (and not simply `state = !state`) to avoid the "double detection" problem stated earlier
+  state = digitalRead(BUTTON);
+  digitalWrite(LED, state);
+  if (state == HIGH) Serial.println("on");
+  else Serial.println("off");
 }
