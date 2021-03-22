69f54dc686bb5e5a4675b44885c11a8ac10414a0
garrettkinman <garrettkinman@gmail.com>
2021-03-22 04:01:40 -0400

    timer works

--- Lab 3/blink_timer_isr/blink_timer_isr.ino
+++ Lab 3/blink_timer_isr/blink_timer_isr.ino
@@ -2,15 +2,30 @@
 
 #define LED 6
 
+volatile byte state = LOW;
+
 void setup() {
   // initialize pins for IO
   pinMode(LED, OUTPUT);
 
-  // for debug purposes
-  Serial.begin(115200);
+  // setup timer
+  cli(); // disable interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0; // init counter to 0
+  OCR1A = 7811; // set counter compare value
+  TCCR1B |= (1 << WGM12); // enable CTC mode
+  TCCR1B |= (1 << CS12)|(1 << CS10); // set prescalar
+  TIMSK1 |= (1 << OCIE1A); // enable timer interrupt
+  sei(); // re-enable interrupts
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+}
 
+// ISR for timer
+ISR(TIMER1_COMPA_vect) {
+  // toggle on/off at every timer interrupt
+  state = !state;
+  digitalWrite(LED, state);
 }
