ff9fa0decf19e5f8141e37538c3132d043864160
garrettkinman <garrettkinman@gmail.com>
2021-03-22 05:50:07 -0400

    70% duty cycle works

--- Lab 3/pwm/pwm.ino
+++ Lab 3/pwm/pwm.ino
@@ -1,28 +1,22 @@
 #include "Arduino_SensorKit.h"
 
-#define LED 6
+#define LED 3
 
 void setup() {
   // initialize pins for IO
+  // set LED as OUTPUT
   pinMode(LED, OUTPUT);
 
-  // setup timer
-  TCCR1A = (TCCR1A & B00111100) | B10000010;
-  TCCR1B = (TCCR1B & B11100000) | B00010001;
-  ICR1 = 0xFFFF;
+  TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM21) | _BV(WGM20);
+  TCCR2B = _BV(CS22);
+  OCR2A = 180;
+  OCR2B = 50;
 }
 
 void loop() {
-  //Increasing pulse
-  for(unsigned int i=0;i<65535;i++){
-    pwm_16(i);
-  }
-  //Decreasing pulse
-  for(unsigned int j=65535;j!=0;j--){
-    pwm_16(j);
-  }
 }
 
-void pwm_16(int_val) {
-  OCR1A = val;
+// val must be 0 to 255
+void pwm(int val) {
+  OCR2A = val;
 }
